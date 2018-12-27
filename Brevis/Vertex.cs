54bbacf0c54f9d11077ae710f758f53d5a09234f
Adrian Sadłocha <adrian.adek@gmail.com>
2018-12-27 17:22:40 +0100

    Add Vertex class

---
+++ Brevis/Vertex.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brevis
+{
+    internal class Vertex
+    {
+        internal readonly double _x;
+        internal readonly double _y;
+        internal readonly double _z;
+        internal Vertex(double x, double y, double z)
+        {
+            this._x = x;
+            this._y = y;
+            this._z = z;
+        }
+    }
+}
