9f8a4103aa062d7330579d589b468381d6458a14
Adrian Sadłocha <adrian.adek@gmail.com>
2018-12-27 17:25:03 +0100

    Add Triangle class

---
+++ Brevis/Triangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brevis
+{
+    internal class Triangle
+    {
+        private readonly Vertex _a;
+        private readonly Vertex _b;
+        private readonly Vertex _c;
+        internal Triangle(Vertex a, Vertex b, Vertex c)
+        {
+            this._a = a;
+            this._b = b;
+            this._c = c;
+        }
+    }
+}
