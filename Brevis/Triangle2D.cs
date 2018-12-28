d88f0e7e14b7b07e6d4bf216e3447a6e09d8b1b9
Adrian Sadłocha <adrian.adek@gmail.com>
2018-12-28 11:23:34 +0100

    Add Triangle2D class

---
+++ Brevis/Triangle2D.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brevis
+{
+    public class Triangle2D
+    {
+        private readonly Vertex2D _a;
+        private readonly Vertex2D _b;
+        private readonly Vertex2D _c;
+
+        public Triangle2D(Vertex2D a, Vertex2D b, Vertex2D c)
+        {
+            this._a = a;
+            this._b = b;
+            this._c = c;
+        }
+    }
+}
