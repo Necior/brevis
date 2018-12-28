2b252f0c0716ce14e2670ce2699d2c8265970b3c
Adrian Sadłocha <adrian.adek@gmail.com>
2018-12-28 11:22:25 +0100

    Add Vertex2D class

---
+++ Brevis/Vertex2D.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brevis
+{
+    public class Vertex2D
+    {
+        private readonly double x_;
+        private readonly double y_;
+
+        public Vertex2D(double x, double y)
+        {
+            this.x_ = x;
+            this.y_ = y;
+        }
+    }
+}
