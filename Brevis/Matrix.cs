a761834405b5ae57f91c649c29fa1a5b60712955
Adrian Sadłocha <adrian.adek@gmail.com>
2018-12-29 12:33:09 +0100

    Add Matrix class

---
+++ Brevis/Matrix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brevis
+{
+    public class Matrix
+    {
+        public readonly double[,] _matrix;
+        public Matrix(int rows, int columns)
+        {
+            this._matrix = new double[rows,columns];
+        }
+
+        public void SetValue(int row, int column, double value)
+        {
+            this._matrix[row, column] = value;
+        }
+
+        public double GetValue(int row, int column)
+        {
+            return this._matrix[row, column];
+        }
+    }
+}
---
+++ BrevisTests/MatrixTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Brevis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brevis.Tests
+{
+    [TestClass()]
+    public class MatrixTests
+    {
+        [TestMethod()]
+        public void GetValueTest()
+        {
+            var m = new Matrix(1, 1);
+            m.SetValue(0, 0, 5);
+            Assert.AreEqual(5, m.GetValue(0, 0));
+            m.SetValue(0, 0, 5);
+            Assert.AreEqual(5, m.GetValue(0, 0));
+            m.SetValue(0, 0, 10);
+            Assert.AreEqual(10, m.GetValue(0, 0));
+        }
+    }
+}
